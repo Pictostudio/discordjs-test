6c72cf06f14a474790cd87a06d632dd0674f9725
Jordan Picton <jordan_picton@hotmail.com>
2023-07-03 12:08:45 +0100

    Initial Push for Bot

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+// Client = Bot
+const { Client, IntentsBitField } = require("discord.js");
+
+// Intents are like a set of permissions that the bot can use to get access to a set of events.
+const client = new Client({
+  intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`✔️ ${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'hello') {
+        message.reply('Hey!');
+    }
+});
+
+client.login(process.env.TOKEN)
